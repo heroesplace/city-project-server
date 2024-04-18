67d8e9ded8cbc97dd9ec260c3027d2e9f0ecd297
Soleo <devsoleo@protonmail.com>
2024-04-18 15:05:24 +0200

    feat: auto-disconnect player

--- src/api/socket/index.js
+++ src/api/socket/index.js
@@ -1,33 +1,48 @@
 const auth = require('../../auth')
 const events = require('./events').events
 
-const authSocketMiddleware = (socket, next) => {
-    const cookie_token = socket.handshake.headers.cookie.split("; ").filter((cookie) => {
-        return cookie.startsWith("token=")
-    })[0].split("=")[1]
+let connections = []
 
-    const token = cookie_token ||  socket.handshake.auth.token
-
-    try {
-        if (!token) {
-            throw new Error('No token provided')
+const destroyPreviousSession = (io, socket) => {
+    io.sockets.sockets.forEach((s) => {
+        if (connections[socket.character_id] == s.id) {
+            console.log('[socket] Déconnexion de la session précédente.')
+            s.disconnect()
         }
-    } catch (err) {
-        return next(err)
-    }
-    
-    auth.verifyTokenAuthenticity(token).then((decoded) => {
-        socket.account_id = decoded.account_id
-        socket.account_name = decoded.account_name
-        socket.character_name = decoded.character_name
-        socket.character_id = decoded.character_id
-
-        socket.join(decoded.character_id)
-
-        next()
-    }).catch(err => {
-        return next(err)
     })
+
+    connections[socket.character_id] = socket.id
+
+    console.log(connections)
+}
+
+const authSocketMiddleware = (socket, next) => {
+    let token = socket.handshake.auth.token
+
+    if (socket.handshake.headers.cookie) {
+        const cookie_token = socket.handshake.headers.cookie.split("; ").filter((cookie) => {
+            return cookie.startsWith("token=")
+        })[0].split("=")[1]
+
+        token = cookie_token
+    }
+
+    if (token) {
+        auth.verifyTokenAuthenticity(token).then((decoded) => {
+            socket.account_id = decoded.account_id
+            socket.account_name = decoded.account_name
+            socket.character_name = decoded.character_name
+            socket.character_id = decoded.character_id
+    
+            socket.join(decoded.character_id)
+    
+            next()
+        }).catch(err => {
+            return next(err)
+        })
+    } else {
+        console.log('[socket] No token provided.')
+    }
 }
 
 const listen = (io, callback) => {
@@ -35,11 +50,11 @@ const listen = (io, callback) => {
 
     callback()
 
-    io.use((socket, next) => {
-        authSocketMiddleware(socket, next)
-    })
+    io.use((socket, next) => authSocketMiddleware(socket, next))
 
     io.on('connection', (socket) => {
+        destroyPreviousSession(io, socket)
+        
         console.log('[socket] Nouvelle connexion établie.')
 
         for (const [event, handler] of Object.entries(events)) {
