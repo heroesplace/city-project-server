822c45e70430e36b695722cedba54b44eeae1464
Soleo <devsoleo@protonmail.com>
2024-05-17 21:36:06 +0200

    feat: add void to map generation system

--- src/api/socket/features/map.js
+++ src/api/socket/features/map.js
@@ -1,93 +1,81 @@
 import fs from 'fs'
 
-const map = JSON.parse(fs.readFileSync('./private/largemap.json', 'utf8')).layers[0].data
+const map = JSON.parse(fs.readFileSync('./private/new_map.json', 'utf8')).layers[0].data
 
 // FACTORISER CETTE FONCTION
 const getMapPart = (x, y, direction) => {
   x = parseInt(x)
   y = parseInt(y)
 
-  const border = 1000
   const frame = { width: 32, height: 18 }
+  const columns = []
 
-  const coordsIndex = parseInt(y * border + x)
-  const indexes = []
-
-  switch (direction) {
-    case "left": {
-      const f = 0 - (frame.width / 2)
-      const topCorner = coordsIndex + f - (frame.height / 2) * border
-
-      for (let i = 0; i < frame.height; i++) {
-        indexes.push(topCorner + (i * border))
-      }
-
-      break
-    }
-
-    case "right": {
-      const f = (frame.width / 2) - 1
-      const topCorner = coordsIndex + f - (frame.height / 2) * border
-
-      for (let i = 0; i < frame.height; i++) {
-          indexes.push(topCorner + (i * border))
-      }
-
-      break
-    }
-
-    case "up": {
-      const f = 0 - (frame.width / 2)
-      const topCorner = coordsIndex + f - (frame.height / 2) * border
-
-      for (let i = 0; i < frame.width; i++) {
-        indexes.push(topCorner + i)
-      }
-
-      break
-    }
-
-    case "down": {
-      const f = 0 - (frame.width / 2) 
-      const bottomCorner = coordsIndex + f + ((frame.height / 2) - 1) * border
-
-      for (let i = 0; i < frame.width; i++) {
-        indexes.push(bottomCorner + i)
-      }
-
-      break
-    }
+  const frameStart = {
+    x: x - Math.floor(frame.width / 2) + 1, 
+    y: y - Math.floor(frame.height / 2) + 1
   }
 
-  return indexes.map((index) => map[index])
+  const frameEnd = {
+    x: frameStart.x + frame.width - 1,
+    y: frameStart.y + frame.height - 1
+  }
+
+  if (direction == "left") {
+      for (let i = frameStart.y; i <= frameEnd.y; i++) {
+        columns.push([i, frameStart.x])
+      }
+  }
+
+  if (direction == "right") {
+      for (let i = frameStart.y; i <= frameEnd.y; i++) {
+        columns.push([i, frameEnd.x])
+      }
+  }
+
+  if (direction == "up") {
+      for (let i = frameStart.x; i <= frameEnd.x; i++) {
+        columns.push([frameStart.y, i])
+      }
+  }
+
+  if (direction == "down") {
+      for (let i = frameStart.x; i <= frameEnd.x; i++) {
+        columns.push([frameEnd.y, i])
+      }
+  }
+
+  return columns.map(([y, x]) => {
+    if (x > 0 && y > 0) return map[y][x]
+
+    return -1
+  })
 }
 
 const getMapFrame = (x, y) => {
-  const border = 1000
-
   const frame = { width: 32, height: 18 }
+  const columns = []
 
-  const frameStartX = Math.max(0, x - Math.floor(frame.width / 2))
-  const frameStartY = Math.max(0, y - Math.floor(frame.height / 2))
-
-  const maxStartX = Math.max(0, border - frame.width)
-  const maxStartY = Math.max(0, border - frame.height)
-
-  const startX = Math.min(frameStartX, maxStartX)
-  const startY = Math.min(frameStartY, maxStartY)
-
-  let path = []
-  for (let row = 0; row < frame.height; row++) {
-    // Convertis la coordonée en index de la liste
-    const rowStartIndex = (startY + row) * border + startX
-
-    const line = Array.from({ length: frame.width }, (_, index) => (rowStartIndex + index))
-    path = path.concat(line)
+  const frameStart = {
+      x: x - Math.floor(frame.width / 2) + 1, 
+      y: y - Math.floor(frame.height / 2) + 1
   }
 
-  const mapFrame = path.map((index) => map[index])
+  const frameEnd = {
+      x: frameStart.x + frame.width - 1,
+      y: frameStart.y + frame.height - 1
+  }
 
-  return mapFrame
+  for (let i = frameStart.y; i <= frameEnd.y; i++) {
+    for (let j = frameStart.x; j <= frameEnd.x; j++) {
+      columns.push([i, j])
+    }
+  }
+
+  return columns.map(([y, x]) => {
+    if (x > 0 && y > 0) return map[y][x]
+
+    return -1
+  })
 }
 
 export { getMapFrame, getMapPart }
